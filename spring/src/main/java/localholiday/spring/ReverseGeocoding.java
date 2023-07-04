a5eac6658d2da6f24b8bfcef28896590e00bde7d
submemb <sons6488@gmail.com>
2023-07-04 22:09:44 +0900

    [FEAT] 리버스 지오코딩 주소 얻기(#25)

--- spring/src/main/java/localholiday/spring/ReverseGeocoding.java
+++ spring/src/main/java/localholiday/spring/ReverseGeocoding.java
@@ -11,33 +11,43 @@ import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class ReverseGeocoding {
 
-    public JsonNode get(String lat, String lon){
+    public JsonNode getJSON(String lat, String lon){
         try{
-            String apiURL = "https://dapi.kakao.com/v2/local/geo/coord2address.json";
+            String apiURL = "https://dapi.kakao.com/v2/local/geo/coord2address.json?";
+            String auth = System.getenv("KAKAO_API_KEY");
             HttpClient client = HttpClientBuilder.create().build();
-            HttpGet getRequest = new HttpGet(apiURL + "?x=126.6244837546&y=37.1686580243");
-            getRequest.setHeader("Authorization","KakaoAK 3a92293484e6b80d6b4910b9937c7a31");
-            log.info("getRequest={}", getRequest);
-            log.info("header={}",getRequest.getHeaders("Authorization"));
+            HttpGet getRequest = new HttpGet(apiURL + "x="+lon+"&y="+lat);
+            getRequest.setHeader("Authorization","KakaoAK "+auth);
+            log.debug("getRequest={}", getRequest);
+            log.debug("header={}",getRequest.getHeaders("Authorization"));
             HttpResponse response = client.execute(getRequest);
             if(response.getStatusLine().getStatusCode() == 200){
                 ResponseHandler<String> handler = new BasicResponseHandler();
                 String body = handler.handleResponse(response);
                 ObjectMapper mapper = new ObjectMapper();
                 JsonNode bodyJson = mapper.readTree(body);
-                log.info("json={}", bodyJson);
                 return bodyJson;
             } else {
                 log.info("error={}", response.getStatusLine().getStatusCode());
+                return null;
             }
 
         }catch (Exception e){
             log.info("error: {}", e.toString());
             return null;
+        }
+    }
+
+    public List<JsonNode> getAddr(String lat, String lon){
+        JsonNode body = getJSON(lat, lon);
+        if(!body.isNull()){
+            return body.findValues("address_name");
         }
         return null;
     }
