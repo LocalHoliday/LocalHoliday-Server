ed3e705f2bebfbadd71bbc19b00da2c30abdaa2a
Son SW <sons69@naver.com>
2023-07-05 20:40:48 +0900

    [FEAT] 스웨거 세팅 파일(#31)

---
+++ spring/src/main/java/localholiday/spring/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package localholiday.spring.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@SecurityScheme(
+        name="accessToken",
+        type= SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@Configuration
+public class SwaggerConfig {
+}
